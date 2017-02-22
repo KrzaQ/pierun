dd3ab74ec5e96f58dccc16a7f4afaf13bdd5b337
krzaq <git@krzaq.cc>
2017-02-22 16:23:00 +0100

    Modularize views.

--- source/pierun/webinterface.d
+++ source/pierun/webinterface.d
@@ -4,26 +4,47 @@ import vibe.d;
 
 import hibernated.core;
 
+alias DBSession = hibernated.session.Session;
+
+struct SessionData
+{
+    bool loggedIn = false;
+    string userName;
+    string sessionId;
+}
+
 class WebInterface
 {
-    this(DataSource, SessionFactory)
-    {
+    private {
+        DataSource dataSource;
+        DBSession session;
+        SessionVar!(SessionData, "session") sessdionData;
+    }
 
+    this(DataSource ds, DBSession s)
+    {
+        this.dataSource = ds;
+        this.session = session;
     }
     
     void index()
     {
         auto code = "";
-        render!("index.dt", code);
+        auto time = "";
+        render!("index.dt", code, time);
     }
 
-    void post(string code)
+    void post(HTTPServerRequest req, string code)
     {
         import pierun.utils.markdown;
+        import std.conv;
 
         code = pierun.utils.markdown.parseMarkdown(code);
 
-        render!("index.dt", code);
+        auto time = (Clock.currTime - req.timeCreated).to!string;
+
+        render!("index.dt", code, time);
     }
 
+    mixin PrivateAccessProxy;
 }
