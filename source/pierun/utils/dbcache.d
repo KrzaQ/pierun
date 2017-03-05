c67c5d921858ed4dec77e6a47c406dbfd0220947
krzaq <krzaq@krzaq.cc>
2017-03-05 16:07:22 +0100

    Moved classes around, KeyValue store uses the new format.

--- source/pierun/utils/dbcache.d
+++ source/pierun/utils/dbcache.d
@@ -8,134 +8,18 @@ import pierun.core, pierun.utils.misc;
 
 alias DBSession = hibernated.session.Session;
 
-struct DBElementDriver(Value, string Key)
+template Cache(Value, string id = "id")
 {
-    alias ValueType = Value;
-    alias KeyType = typeof(mixin(Value.stringof ~ "." ~ Key));
-
-    static ValueType get(DBSession session, const KeyType key)
-    {
-        ValueType v = session
-            .createQuery("FROM " ~ ValueType.stringof ~
-                " WHERE " ~ Key ~ "=:Key")
-            .setParameter("Key", key)
-            .uniqueResult!ValueType;
-        return v;
-    }
-
-    static void set(DBSession session, ValueType value)
-    {
-        session.save(value);
-    }
-
-    static void update(DBSession session, ValueType value)
-    {
-        session.update(value);
-    }
-
-    static void remove(DBSession session, ValueType value)
-    {
-        session.remove(value);
-    }
-}
-
-struct CacheElement(Driver, int IdealValuesCount = 1024)
-{
-    import std.datetime;
-
-    alias ValueType = Driver.ValueType;
-    alias KeyType = Driver.KeyType;
-
-    this(DBSession session)
-    {
-        this.session = session;
-    }
-
-    ValueType get(const KeyType key)
-    {
-        auto ptr = key in data;
-
-        if(ptr !is null) {
-            ptr.lastAccessed = Clock.currTime;
-            return ptr.value;
-        }
-
-        auto value = Driver.get(session, key);
-
-        if(value !is null) {
-            data[key] = Element(value);
-        }
-
-        return value;
-    }
-
-    void set(const KeyType key, ValueType value)
-    {
-        Driver.set(session, value);
-    }
-
-    void reset()
-    {
-        data.clear;
-    }
-
-    void reset(const KeyType value)
-    {
-        data.remove(value);
-    }
-
-    void remove(ValueType value)
-    {
-        Driver.remove(session, value);
-    }
-
-    void update(ValueType value)
-    {
-        Driver.update(session, value);
-    }
-
-    void gc()
-    {
-        if(data.length < 2 * IdealValuesCount)
-            return;
-
-        import std.algorithm, std.array, std.range;
-        auto toRemove = data.byKeyValue
-            .array
-            .sort!((a,b) => a.value.lastAccessed > b.value.lastAccessed)
-            .drop(cast(int)(IdealValuesCount * 0.75))
-            .map!(e => e.key);
-
-        foreach(k; toRemove) {
-            data.remove(k);
-        }
-    }
-
-    private struct Element
-    {
-        this(ValueType value)
-        {
-            this.value = value;
-            this.lastAccessed = Clock.currTime;
-        }
-
-        ValueType value;
-        SysTime lastAccessed;
-    }
-
-    private {
-        DBSession session;
-        Element[KeyType] data;
-    }
+    alias Cache = CacheElement!(DBElementDriver!(Value, id));
 }
 
 class DBCache
 {
     private {
         DBSession session;
-        CacheElement!(DBElementDriver!(Post, "id")) posts;
-        //Post[int] posts;
-        KeyValue[string] keyValues;
+        //CacheElement!(DBElementDriver!(Post, "id")) posts;
+        Cache!Post posts;
+        Cache!(KeyValue, "key") keyValues;
         Tag[string] tags;
         Language[string] languages;
     }
@@ -144,26 +28,13 @@ class DBCache
     {
         this.session = s;
         posts = typeof(posts)(s);
+        keyValues = typeof(keyValues)(s);
     }
 
     auto getPost(int id)
     {
         return posts.get(id);
     }
-
-    //auto getPost(int id)
-    //{
-    //    auto ptr = id in posts;
-    //    if(ptr !is null) return *ptr;
-
-    //    Post p = session.createQuery("FROM Post WHERE id=:Id")
-    //        .setParameter("Id", id)
-    //        .uniqueResult!Post;
-
-    //    if(p !is null)
-    //        posts[id] = p;
-    //    return p;
-    //}
 
     Language getLanguage(const string code)
     {
@@ -231,41 +102,178 @@ class DBCache
 
     KeyValue getValue(const string key)
     {
-        auto ptr = key.toLower in keyValues;
-        if(ptr !is null) {
-            return *ptr;
-        }
+        return keyValues.get(key);
+        //auto ptr = key.toLower in keyValues;
+        //if(ptr !is null) {
+        //    return *ptr;
+        //}
 
-        KeyValue kv = session
-            .createQuery("FROM KeyValue WHERE key=:Key")
-            .setParameter("Key", key.toLower)
-            .uniqueResult!KeyValue;
+        //KeyValue kv = session
+        //    .createQuery("FROM KeyValue WHERE key=:Key")
+        //    .setParameter("Key", key.toLower)
+        //    .uniqueResult!KeyValue;
 
-        if(kv !is null) {
-            keyValues[key.toLower] = kv;
-        }
+        //if(kv !is null) {
+        //    keyValues[key.toLower] = kv;
+        //}
 
-        return kv;
+        //return kv;
     }
 
     void setValue(T)(const string key, const T value)
     {
-        keyValues.remove(key.toLower);
+        auto updater = delegate (KeyValue v) => v.value = value.to!string;
+        keyValues.setOrUpdate(key, updater);
+        //keyValues.remove(key.toLower);
         
-        KeyValue kv = session
-            .createQuery("FROM KeyValue WHERE key=:Key")
-            .setParameter("Key", key.toLower)
-            .uniqueResult!KeyValue;
+        //KeyValue kv = session
+        //    .createQuery("FROM KeyValue WHERE key=:Key")
+        //    .setParameter("Key", key.toLower)
+        //    .uniqueResult!KeyValue;
 
-        if(kv is null) {
-            kv = new KeyValue;
-            kv.key = key.toLower;
-            kv.value = value.to!string;
-            session.save(kv);
-            keyValues[kv.key] = kv;
-        } else {
-            kv.value = value.to!string;
-            session.update(kv);
+        //if(kv is null) {
+        //    kv = new KeyValue;
+        //    kv.key = key.toLower;
+        //    kv.value = value.to!string;
+        //    session.save(kv);
+        //    keyValues[kv.key] = kv;
+        //} else {
+        //    kv.value = value.to!string;
+        //    session.update(kv);
+        //}
+    }
+}
+
+private struct DBElementDriver(Value, string Key)
+{
+    alias ValueType = Value;
+    alias KeyType = typeof(mixin(Value.stringof ~ "." ~ Key));
+
+    static ValueType get(DBSession session, const KeyType key)
+    {
+        ValueType v = session
+            .createQuery("FROM " ~ ValueType.stringof ~
+                " WHERE " ~ Key ~ "=:Key")
+            .setParameter("Key", key)
+            .uniqueResult!ValueType;
+        return v;
+    }
+
+    static void set(DBSession session, ValueType value)
+    {
+        session.save(value);
+    }
+
+    static void update(DBSession session, ValueType value)
+    {
+        session.update(value);
+    }
+
+    static void remove(DBSession session, ValueType value)
+    {
+        session.remove(value);
+    }
+}
+
+private struct CacheElement(Driver, int IdealValuesCount = 1024)
+{
+    import std.datetime;
+
+    alias ValueType = Driver.ValueType;
+    alias KeyType = Driver.KeyType;
+
+    this(DBSession session)
+    {
+        this.session = session;
+    }
+
+    ValueType get(const KeyType key)
+    {
+        auto ptr = key in data;
+
+        if(ptr !is null) {
+            ptr.lastAccessed = Clock.currTime;
+            return ptr.value;
         }
+
+        auto value = Driver.get(session, key);
+
+        if(value !is null) {
+            data[key] = Element(value);
+        }
+
+        return value;
+    }
+
+    void reset()
+    {
+        data.clear;
+    }
+
+    void reset(const KeyType value)
+    {
+        data.remove(value);
+    }
+
+    void remove(ValueType value)
+    {
+        Driver.remove(session, value);
+    }
+
+    void set(ValueType value)
+    {
+        Driver.set(session, value);
+    }
+
+    void update(ValueType value)
+    {
+        Driver.update(session, value);
+    }
+
+    void setOrUpdate(Updater)(KeyType key, Updater updater)
+    {
+        auto value = this.get(key);
+        if(value is null) {
+            value = new ValueType;
+            updater(value);
+            this.set(value);
+        } else {
+            updater(value);
+            this.update(value);
+        }
+    }
+
+    void gc()
+    {
+        if(data.length < 2 * IdealValuesCount)
+            return;
+
+        import std.algorithm, std.array, std.range;
+        auto toRemove = data.byKeyValue
+            .array
+            .sort!((a,b) => a.value.lastAccessed > b.value.lastAccessed)
+            .drop(cast(int)(IdealValuesCount * 0.75))
+            .map!(e => e.key);
+
+        foreach(k; toRemove) {
+            data.remove(k);
+        }
+    }
+
+    private struct Element
+    {
+        this(ValueType value)
+        {
+            this.value = value;
+            this.lastAccessed = Clock.currTime;
+        }
+
+        ValueType value;
+        SysTime lastAccessed;
+    }
+
+    private {
+        DBSession session;
+        Element[KeyType] data;
     }
 }
