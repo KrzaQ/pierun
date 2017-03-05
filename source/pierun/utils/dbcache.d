a601348d72dea7c61bff35a0ab5530febec02aab
krzaq <krzaq@krzaq.cc>
2017-03-05 13:39:49 +0100

    dbcache refactoring - alpha

--- source/pierun/utils/dbcache.d
+++ source/pierun/utils/dbcache.d
@@ -8,10 +8,132 @@ import pierun.core, pierun.utils.misc;
 
 alias DBSession = hibernated.session.Session;
 
+struct DBElementDriver(Value, string Key)
+{
+    alias ValueType = Value;
+    alias KeyType = typeof(mixin(Value.stringof ~ "." ~ Key));
+
+    static ValueType get(DBSession session, const KeyType key)
+    {
+        ValueType v = session
+            .createQuery("FROM " ~ ValueType.stringof ~
+                " WHERE " ~ Key ~ "=:Key")
+            .setParameter("Key", key)
+            .uniqueResult!ValueType;
+        return v;
+    }
+
+    static void set(DBSession session, ValueType value)
+    {
+        session.save(value);
+    }
+
+    static void update(DBSession session, ValueType value)
+    {
+        session.update(value);
+    }
+
+    static void remove(DBSession session, ValueType value)
+    {
+        session.remove(value);
+    }
+}
+
+struct CacheElement(Driver, int IdealValuesCount = 1024)
+{
+    import std.datetime;
+
+    alias ValueType = Driver.ValueType;
+    alias KeyType = Driver.KeyType;
+
+    this(DBSession session)
+    {
+        this.session = session;
+    }
+
+    ValueType get(const KeyType key)
+    {
+        auto ptr = key in data;
+
+        if(ptr !is null) {
+            ptr.lastAccessed = Clock.currTime;
+            return ptr.value;
+        }
+
+        auto value = Driver.get(session, key);
+
+        if(value !is null) {
+            data[key] = Element(value);
+        }
+
+        return value;
+    }
+
+    void set(const KeyType key, ValueType value)
+    {
+        Driver.set(session, value);
+    }
+
+    void reset()
+    {
+        data.clear;
+    }
+
+    void reset(const KeyType value)
+    {
+        data.remove(value);
+    }
+
+    void remove(ValueType value)
+    {
+        Driver.remove(session, value);
+    }
+
+    void update(ValueType value)
+    {
+        Driver.update(session, value);
+    }
+
+    void gc()
+    {
+        if(data.length < 2 * IdealValuesCount)
+            return;
+
+        import std.algorithm, std.array, std.range;
+        auto toRemove = data.byKeyValue
+            .array
+            .sort!((a,b) => a.value.lastAccessed > b.value.lastAccessed)
+            .drop(IdealValuesCount)
+            .map!(e => e.key);
+
+        foreach(k; toRemove) {
+            data.remove(k);
+        }
+    }
+
+    private struct Element
+    {
+        this(ValueType value)
+        {
+            this.value = value;
+            this.lastAccessed = Clock.currTime;
+        }
+
+        ValueType value;
+        SysTime lastAccessed;
+    }
+
+    private {
+        DBSession session;
+        Element[KeyType] data;
+    }
+}
+
 class DBCache
 {
     private {
         DBSession session;
+        CacheElement!(DBElementDriver!(Post, "id")) posts_;
         Post[int] posts;
         KeyValue[string] keyValues;
         Tag[string] tags;
@@ -21,6 +143,7 @@ class DBCache
     this(DBSession s)
     {
         this.session = s;
+        posts_ = typeof(posts_)(s);
     }
 
     auto getPost(int id)
